1f89ef2ec17a7e5ccde123e43028ce2af9ab9ebd
kreloaded <kiran@ost.com>
2020-04-03 17:25:13 +0530

    Moved mysql to db

--- src/api/farmer.js
+++ src/api/farmer.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const MySQL = require('./mysql_farmer');
+const MySQL = require('../db/mysql');
 
 // use cors to allow cross origin resource sharing.
 app.use(
