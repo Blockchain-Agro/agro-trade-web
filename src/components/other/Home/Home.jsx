906b8f8ed3a89c08298b018210ffafd1b3c4c9ae
kreloaded <kiran@ost.com>
2020-04-08 19:21:39 +0530

    Added blocks on home page with images

--- src/components/other/Home/Home.jsx
+++ src/components/other/Home/Home.jsx
@@ -5,21 +5,53 @@ import NavbarHome from './NavbarHome.js';
 export default class Home extends Component {
   render () {
     return (
-     <div className="bg">
+     <div className="home">
         <NavbarHome/>
-        <label  class="agro">Agrotrade</label>
-        <h1 class="pad">A Blockchain Based Decentralized Platform for Trading Agricultural Products</h1>
+        <label  className="agro">Agrotrade</label>
+        <h1 className="pad">A Blockchain Based Decentralized Platform for Trading Agricultural Products</h1>
         <div className="network-image" />
-        <div>
-            <h2>Welcome to the world of blockchains</h2>
-            <p>Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum.</p>
+        <div className="container">
+            <div className="introduction">
+                <h2>Welcome to the world of blockchains</h2>
+                <p>Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                    anim id est laborum.
+                </p>
+            </div>
+            <h1>Description</h1>
+            <div className="row">
+                <div className="col-sm-6">
+                    <p>Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                </div>
+                <div className="col-sm-6 first-image"/>
+            </div>
+            <div className="row">
+                <div className="col-sm-6 first-image"/>
+                <div className="col-sm-6">
+                    <p>Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                </div>
+            </div>
         </div>
      </div>
     );
