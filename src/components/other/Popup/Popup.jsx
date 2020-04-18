20e320050d3c596766e21921fdac68cb80e59a15
kreloaded <kiran@ost.com>
2020-04-18 12:14:55 +0530

    farmer phone number issue solved

--- src/components/other/Popup/Popup.jsx
+++ src/components/other/Popup/Popup.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
 import { Component } from 'react';
+import Loader from '../Loader/Loader';
 
 class PopupComponent extends Component {
     render() {
         return (
             <Popup trigger={<button> Trigger</button>} position="right center">
+                <Loader />
                 <div>Popup content here !!</div>
             </Popup>
         );
