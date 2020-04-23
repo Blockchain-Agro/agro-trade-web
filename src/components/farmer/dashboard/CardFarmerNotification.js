821063098517cbb2cde15c45d0141b438addb8b9
rajat81 <rajatswnt@gmail.com>
2020-04-23 17:31:28 +0530

    home

--- src/components/farmer/dashboard/CardFarmerNotification.js
+++ src/components/farmer/dashboard/CardFarmerNotification.js
@@ -1,69 +1,12 @@
-/*
 import React, { Component } from 'react';
 import { Card, CardText, CardBody, Button,
   CardTitle, CardSubtitle } from 'reactstrap';
 import './../style.css'
-
-
 class PeopleCard extends Component {
   constructor(props) {
     super(props);
-this.shoot = this.shoot.bind(this);
+    this.shoot = this.shoot.bind(this);
   }
-
-  shoot()
-  {
-          this.props.history.push({
-        pathname: '../productInfoNotification',
-        state: {
-            id: this.props.person.id,
-            product_name: this.props.person.product_name,
-            type_crop:this.props.person.type_crop,
-            price_per_kg:this.props.person.price_per_kg,
-            quantity_in_kg:this.props.person.quantity_in_kg,
-            expiry_date:this.props.person.expiry_date,
-            vendor_contact:this.props.person.vendor_contact,
-            vendor_name:this.props.person.vendor_name
-        }
-    });
-  }
-
-
-  render () {
-let { product_name , type_crop, price_per_kg, quantity_in_kg,expiry_date } = this.props.person;
-    return (
-        <div>
-        <Card>
-          <CardBody>
-            <CardTitle>Name : {product_name}</CardTitle>
-            <CardSubtitle>Type : {type_crop}</CardSubtitle>
-            <CardText>Price : {price_per_kg}</CardText>
-            <CardText>Quantity : {quantity_in_kg}</CardText>
-            <CardText>Expiry Date : {expiry_date}</CardText>
-            <Button history= {this.props.history} onClick={this.shoot}>View Info</Button>
-          </CardBody>
-        </Card>
-      </div>
-    )
-  }
-}
-
-export default PeopleCard;
-
-*/
-
-import React, { Component } from 'react';
-import { Card, CardText, CardBody, Button,
-  CardTitle, CardSubtitle } from 'reactstrap';
-import './../style.css'
-
-
-class PeopleCard extends Component {
-  constructor(props) {
-    super(props);
-this.shoot = this.shoot.bind(this);
-  }
-
   shoot()
   {
           this.props.history.push({
@@ -79,8 +22,6 @@ this.shoot = this.shoot.bind(this);
         }
     });
   }
-
-
   render () {
 let { name , first_name, type, price, quantity, phone_number } = this.props.person;
     return (
--- src/components/other/Home/Home.jsx
+++ src/components/other/Home/Home.jsx
@@ -8,47 +8,44 @@ export default class Home extends Component {
      <div className="home">
         <Navbar/>
         <label  className="agro">AgroTrade</label>
-        <h1 className="pad">A Blockchain Based Platform for Trading Agricultural Products</h1>
+        <h1 style={{marginBottom:'50px'}} className="pad">A Blockchain Based Platform for Trading Agricultural Products</h1>
         <div className="network-image" />
-        <div className="container">
-            <div className="introduction">
-                <h2>Welcome to the world of blockchains</h2>
-                <p>Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                    cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                    anim id est laborum.
+        <div  className="container">
+            <div className="introduction" style={{justifyContent: 'center',
+alignItems: 'center', textAlign: 'left'}}>
+                <h2 style={{marginBottom:'30px'}}>Welcome to the world of blockchains</h2>
+                <p >A blockchain, originally block chain, is a growing list of records, called blocks, that are linked using cryptography. Each block contains a cryptographic hash of the previous block, a timestamp, and transaction data (generally represented as a Merkle tree). By design, a blockchain is resistant to modification of the data. It is "an open, distributed ledger that can record transactions between two parties efficiently and in a verifiable and permanent way". For use as a distributed ledger, a blockchain is typically managed by a peer-to-peer network collectively adhering to a protocol for inter-node communication and validating new blocks. Once recorded, the data in any given block cannot be altered retroactively without alteration of all subsequent blocks, which requires consensus of the network majority. Although blockchain records are not unalterable, blockchains may be considered secure by design and exemplify a distributed computing system with high Byzantine fault tolerance. Decentralized consensus has therefore been claimed with a blockchain.
                 </p>
             </div>
             <h1>Description</h1>
             <div className="row">
                 <div className="col-sm-6">
-                    <p>Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum.
-                    </p>
+                    <p style={{justifyContent: 'center',
+alignItems: 'center', textAlign: 'left'}}>The Farmers are not satisfied with the process of the agricultural products
+trading. Farmers do not have rights to set the price of their product as per cur-
+rent process. A Centralised platform can be made to connect farmers to vendors
+and facilitate farmers to set the price for their product but issues like consumer
+trust are the reliability issues which are faced by current supply chains. Hence
+preparing a transparent and efficient architecture for trading agricultural products
+was the purpose of the study, by combining trusted certification system utilizing
+blockchain concept and other technologies.
+</p>
                 </div>
                 <div className="col-sm-6 first-image"/>
             </div>
             <div className="row">
                 <div className="col-sm-6 first-image"/>
                 <div className="col-sm-6">
-                    <p>Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum.
+                    <p style={{justifyContent: 'center',
+alignItems: 'center', textAlign: 'left'}}>In the Agri-Food domain, in order to maintain trust and reliability along the whole
+supply chain, it is essential for the stored records to be tamper-proof, while the
+best case would be if each actor issuing transactions could do that without relying
+on any centralized third-party intermediary. A potential solution to all
+of such issues and concerns is the Block chain technology, which is a peer-to-peer
+digital ledger that does not rely on centralized servers. Since all the records stored
+in a block chain are based on a consensus reached at least by the absolute majority
+of peers of the network itself, this distributed ledger is immutable by design and
+offers an audible and transparent source of information.
                     </p>
                 </div>
             </div>
